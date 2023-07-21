1bcaae42bdd2fb29c63e38d41da53c2359d9d002
Mihail Kiyamov <mihailkiyamov@yandex.ru>
2023-07-21 11:52:35 +0500

    sprint15-try2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,12 +4,14 @@ const User = require('../models/users');
 const NotFoundError = require('../errors/not-found-err');
 const WrongDataError = require('../errors/wrong-data-err');
 
+const { JWT_SECRET } = process.env;
+
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'mesto-backend', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 * 24 * 7 }).send({});
     })
     .catch(next);
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-err');
 
+const { JWT_SECRET } = process.env;
+
 module.exports.auth = (req, res, next) => {
   const token = req.cookies.jwt;
+  if (!token) next(new AuthError('Отсутствует токен'));
 
   let payload;
 
   try {
-    payload = jwt.verify(token, 'mesto-backend');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     const authError = new AuthError(err.message);
     next(authError);
